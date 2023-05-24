f1edd8e7f956f8c94c955341d58475c2a2fc18f5
nicohermida01 <nshermida101@gmail.com>
2023-05-24 19:08:28 -0300

    add study logo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
+import { StudyLogo } from './components/icons/StudyLogo'
 
 export default function Home() {
 	return (
 		<div className='min-h-screen flex gap-4 flex-col items-center justify-center'>
-			<h1 className='font-bold text-3xl'>Home Page</h1>
+			<StudyLogo width={250} height={70} />
+
+			<h1 className='font-bold text-xl'>Home Page</h1>
 
 			<nav>
 				<ul className='[&>*:hover]:underline [&>*]:text-center'>
