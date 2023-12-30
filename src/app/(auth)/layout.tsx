a6e11b4f7201405c81b773080ffb946fda58f32e
Nico Hermida <hermida.nicolas101@gmail.com>
2023-12-30 18:23:54 -0300

    create support page and add link navigation between login and register pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,26 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function AuthLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	const pathname = usePathname()
+
 	return (
 		<section className='w-full h-screen'>
 			<header className='w-full  fixed'>
 				<nav className='max-w-[1000px] mx-auto flex justify-between font-bold uppercase p-[32px]'>
-					<Link href='#'>Contact</Link>
-					<Link href='#'>Sign up</Link>
+					<Link href='/support'>Contact</Link>
+					<Link href={pathname === '/login' ? '/register' : '/login'}>
+						{pathname === '/login' ? 'Sign up' : 'Log in'}
+					</Link>
 				</nav>
 			</header>
+
 			<main className='grid place-items-center h-full'>{children}</main>
 		</section>
 	)
