69e562335eeac47c59b1e38eb17003a6d4bf4a43
Nico Hermida <hermida.nicolas101@gmail.com>
2024-06-27 19:16:02 -0300

    add api-url into env var

--- src/httpclients/api.ts
+++ src/httpclients/api.ts
@@ -1,12 +1,10 @@
 import axios from 'axios'
 import { setupCache } from 'axios-cache-interceptor'
 
-const API_URL = 'http://docker.localhost:8001/api'
-
 axios.defaults.withCredentials = true
 
 const axiosInstance = axios.create({
-	baseURL: API_URL,
+	baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 	withCredentials: true,
 })
 
